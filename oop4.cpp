3e1eb41e03b2dd8e29b26681048aa4d67f8e38c6
kunal kank <112045799+KUNALKANK1737@users.noreply.github.com>
2022-12-08 20:13:19 +0530

    Create oop4.cpp

---
+++ oop4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(){
+	fstream file;
+	string ch;
+	file.open("sample.txt",ios::out);
+	cout<<"Enter the content of the file :"<<endl;
+    getline(cin,ch);
+	file<<ch;
+	file.close();
+	file.open("sample.txt",ios::in);
+	while(file.eof()==0){
+		getline(cin,ch);
+		cout<<ch<<endl;
+	}
+	file.close();
+
+	}
