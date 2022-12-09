49e7fb7fc82616e2244ed964f51c62e7888c080d
kunal kank <112045799+KUNALKANK1737@users.noreply.github.com>
2022-12-09 08:24:26 +0530

    Update oop4.cpp

--- oop4.cpp
+++ oop4.cpp
@@ -6,14 +6,14 @@ int main(){
 	fstream file;
 	string ch;
 	file.open("sample.txt",ios::out);
-	cout<<"Enter the content of the file :"<<endl;
+	cout<<"Enter the content of the file :";
     getline(cin,ch);
 	file<<ch;
 	file.close();
 	file.open("sample.txt",ios::in);
 	while(file.eof()==0){
 		getline(file,ch);
-		cout<<ch<<endl;
+		cout<<"The data in the file is :"<<ch<<endl;
 	}
 	file.close();
 
